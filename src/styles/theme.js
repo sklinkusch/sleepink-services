0f0f1f24700882ce433bdeac238245656f6838cc
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-23 21:55:38 +0200

    theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  breakpoints: ["576px", "768px", "992px", "1200px"],
+  container: ["540px", "720px", "960px", "1140px"],
+  fonts: {
+    body: `-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`,
+  }
+}
+
+export default theme
