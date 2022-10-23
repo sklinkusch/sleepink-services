0345bff7b11782978a4cb579e0c55a1fd9567baa
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-23 21:57:25 +0200

    include ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'theme-ui';
+import theme from './styles/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
